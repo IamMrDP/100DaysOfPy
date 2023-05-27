68cc10983c93b32f60f1268cd756e3b215a3a9be
Dinith Perera <94979426+IamMrDP@users.noreply.github.com>
2023-05-27 16:08:31 +0530

    Add files via upload
    Print first name and last name in reverse order

---
+++ ReverseName.py
@@ -0,0 +1,9 @@
+"""
+w3resource:
+Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them. 
+
+Date: Sat 27 May 2023 (D1,P3)
+"""
+f=str(input("Enter your first name: "))
+l=str(input("Enter your last name: "))
+print(l,"",f)
